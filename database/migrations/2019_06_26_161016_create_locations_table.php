df62cb9ee667eafa80adb176d03dd10ece6d8bef
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-06-26 17:31:11 +0100

    location table migration created

---
+++ database/migrations/2019_06_26_161016_create_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('postcode');
+            $table->double('longitude',6,2);
+            $table->double('latitude',6,2);
+            $table->string('district');
+            $table->string('county');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
