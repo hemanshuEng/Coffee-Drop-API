71306ba6644297b6cddc4eb3404345118d3ac22a
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-06-26 17:46:19 +0100

    location and timetable model created

---
+++ database/migrations/2019_06_26_163831_create_timetables_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimetablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('location_id');
+            $table->integer('day');
+            $table->time('open')->nullable();
+            $table->time('closed')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetables');
+    }
+}
