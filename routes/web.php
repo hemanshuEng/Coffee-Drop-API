420b3b588223292b17b05d0dc75dbb90d21ac010
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-06-27 08:07:51 +0100

    added readme file

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,6 @@ use Jabranr\PostcodesIO\PostcodesIO;
 
 Route::get('/', function () {
 
-    $days = array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
-    $test = array('sunday' => 19, 'monday' => 20);
-    for ($i = 1; $i < 8; $i++) {
-        $day = $days[$i - 1];
-        if (array_key_exists($day, $test)) {
-            //$timetable->location_id = $location->id;
-            echo $day;
-        } else {
-            //$timetable->location_id = $location->id;
-            echo $day;
-        }
-    }
-    //return view('welcome');
+
+    return view('welcome');
 });
